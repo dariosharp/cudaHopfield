98dac13ff0426250dc039bed699e1546f65bc27c
Dario Bertani <darioemilio.bertani@studenti.unimi.it>
2016-10-27 11:21:38 +0200

    added no tested hopActivation function

--- hopfield.cu
+++ hopfield.cu
@@ -15,11 +15,39 @@ __global__ void training(int dimP, int nP, int *ps, float *ws){
 	int x;
 	x = blockIdx.x*blockDim.x + threadIdx.x;
 	for (int i = 0; i < nP; i++)	
-		s[x] += (float)(ps[i*dimP+(x/dimP)]*ps[i*dimP+(x%dimP)]);
+		s[x] += (float)((2*ps[i*dimP+(x/dimP)]-1)*(2*ps[i*dimP+(x%dimP)]-1));
 	s[((x/dimP)*dimP)+(x/dimP)] = 0.0f;
-	__syncthreads();
+	//__syncthreads();
 	ws[x] = s[x]/nP;
 }
+
+
+__global__ void hopActivation(int *dimP, int *ws, int*pt, int*at){
+	int neuron_index = blockDim.x*blockIdx.x + threadIdx.x;
+	int product = 0;
+	for (int i = 0; i < dimP; i++)
+		product += ws[(x*dimP)+i] * pt[i];
+	activation[neuron_index] = product;
+}
+
+
+/*__device__ __forceinline__ double sigmoid (double a)
+{
+    return 1.0 / (1.0 + exp (-a));
+}
+
+
+__global__ void sigmoid_kernel (const double * __restrict__ src, 
+                                double * __restrict__ dst, int len)
+{
+    int stride = gridDim.x * blockDim.x;
+    int tid = blockDim.x * blockIdx.x + threadIdx.x;
+    for (int i = tid; i < len; i += stride) {
+        dst[i] = sigmoid (src[i]);
+    }
+}   
+*/
+
 
 
 int main(int argc, char *argv[]){
@@ -28,8 +56,8 @@ int main(int argc, char *argv[]){
 	int nPatterns, dimPatterns;
 	//printf("Insert number of patterns: ");
 	//scanf("%d", &num);
-	nPatterns = 2;
-	dimPatterns = 4;
+	nPatterns = 3;
+	dimPatterns = 7;
 
 	int sizeP = dimPatterns*sizeof(int);
 	int sizeW = dimPatterns*dimPatterns*sizeof(float);
@@ -39,7 +67,7 @@ int main(int argc, char *argv[]){
 	cudaMalloc ( &ws, (sizeW));
       
 	for (int i = 0; i < nPatterns*dimPatterns; i++) {
-		patterns[i] = rand() % 2 == 0 ? -1 : 1; 
+		patterns[i] = rand() % 2; 
 	}
 	cudaMemcpy (ps, patterns, sizeP*nPatterns, cudaMemcpyHostToDevice);
 
@@ -57,7 +85,7 @@ int main(int argc, char *argv[]){
 	cudaThreadSynchronize();
   
 	cudaMemcpy (weights, ws, sizeW, cudaMemcpyDeviceToHost);	
-   	printf("C:\n");
+   	printf("Weights:\n");
    	for(int i = 0; i < dimPatterns; i++){
       		printf("[ ");
       		for (int j = 0; j < dimPatterns; j++) {
