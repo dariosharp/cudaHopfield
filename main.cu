5b02e161ecb5ddc0ec165b4def5a5b432bf3cf5e
Dario Bertani <darioemilio.bertani@studenti.unimi.it>
2016-11-28 16:44:13 +0100

    main inline unworked version

--- main.cu
+++ main.cu
@@ -4,29 +4,33 @@
 #include <time.h>
 #include "hopfield.h"
 
-void checkVal(float *matrix, int dimP, int nP, int nC)
+int checkVal(float *weights, int dimPattern, int nPatterns)
 {
-	printf("In check val\n");
-	/*
+	int i = 0;
 	int * epat = (int *)malloc (dimPattern*sizeof(int));
-        epat[0] = 1;
-        epat[1] = 0;
-        epat[2] = 1;
-        epat[3] = 0;
-        epat[4] = 1;
-        epat[5] = 1;
-        epat[6] = 0;
+        
+	srand(time(NULL));
+	printf("Input:\n[");
+        for (i = 0; i < dimPattern; i++) {
+                epat[i] = rand() % 2;
+		printf(" %d", epat[i]);
+        }
+	printf(" ]\n");
 
-        int * activation = actFunc(dimPattern, epat, weights);
-        if (activation == NULL){
+	for (i = 0; i < 60; i++)
+        	epat = actFunc(dimPattern, epat, weights);
+        if (epat == NULL){
                 printf("Error on Activarion\n");
                 return 1;
         }
+
         printf("activation [");
-        for (int i = 0; i < dimPattern; i++)
-                printf("%i ", activation[i]);
+        for (i = 0; i < dimPattern; i++)
+                printf("%i ", epat[i]);
         printf("]\n");
-	*/
+
+	free(epat);
+	return 0;
 }
 
 
@@ -34,7 +38,9 @@ float * randomValue(int nPatterns, int dimPattern)
 {
       	int * patterns;
 	int i, j;
-        if ((patterns = (int*) malloc (dimPattern*nPatterns*sizeof(int))) == NULL ) return NULL;
+        
+	if ((patterns = (int*) malloc (dimPattern*nPatterns*sizeof(int))) == NULL ) return NULL;
+
 	srand(time(NULL));
         for (i = 0; i < nPatterns*dimPattern; i++) {
                 patterns[i] = rand() % 2;
@@ -52,7 +58,6 @@ float * randomValue(int nPatterns, int dimPattern)
                 printf("Error on Learning\n");
                 return NULL;
         }
-
         printf("Weights:\n");
         for(i = 0; i < dimPattern; i++){
                 printf("[ ");
@@ -68,17 +73,18 @@ float * randomValue(int nPatterns, int dimPattern)
 int main(int argc, char *argv[])
 {
         int i, k;
-        int dimP, nP, nC, type = 0;
+        int dimP, nP, type = 0;
 
         if (argc < 2){
-		printf("	-pf [patterns file name] -rf [patterns recognize]					Get pattern from File\n");
-		printf("	--inline -dimP [size one pattern] -nP [number of patterns] -nC [numbre of check]	Generate random Patterns\n");
-		printf("	-h											Help\n");
+		printf("	-pf [patterns file name] -rf [patterns recognize]		Get pattern from File\n");
+		printf("	--inline -dimP [size one pattern] -nP [number of patterns]	Generate random Patterns\n");
+		printf("	-h								Help\n");
 		return 1;	
 	}
 
 	for (i = 1; i < argc; i++){
 		if(strcmp(argv[i], "-pf")==0){
+			type += 0x1
 			printf("Pattens file name: %s\n", argv[++i]);		
 		}
 		if(strcmp(argv[i], "-rf")==0){
@@ -86,15 +92,12 @@ int main(int argc, char *argv[])
 		}
 		if(strcmp(argv[i], "--inline")==0){
 			type += 0x10;
-			for(k = 1; k <= 5; k++){
+			for(k = 1; k <= 3; k++){
 				if(strcmp(argv[i+k], "-dimP")==0){
 					dimP = atoi(argv[i+(++k)]);
 				}
 				if(strcmp(argv[i+k], "-nP")==0){
 					nP = atoi(argv[i+(++k)]);		
-				}
-				if(strcmp(argv[i+k], "-nC")==0){
-					nC = atoi(argv[i+(++k)]);		
 				}
 			}
 			i +=k; 
@@ -105,7 +108,8 @@ int main(int argc, char *argv[])
 		float * matrix = randomValue(nP, dimP);
 		if(matrix == NULL)
 			return 1;
-		checkVal(matrix, dimP, nP, nC);
+		checkVal(matrix, dimP, nP);
+		free(matrix);
 	}
 
 	return 0;
