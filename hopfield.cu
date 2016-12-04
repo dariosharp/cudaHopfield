9bf9523193d90ef67c2ffccc379bf4f721f973b8
Dario Bertani <darioemilio.bertani@studenti.unimi.it>
2016-12-04 21:04:49 +0100

    release 0.2 untested

--- hopfield.cu
+++ hopfield.cu
@@ -9,41 +9,42 @@
 #define sizeBlok 1024
 #define sizeWarp 32
 
+#define imin(a,b) (a<b?a:b)
+
 
 __global__ void training(int dimP, int nP, int *ps, float *ws)
 {
-	extern __shared__ float s[];
+	float product;
 	int x;
 	x = blockIdx.x*blockDim.x + threadIdx.x;
-	s[x] = 0.0f;
-	for (int i = 0; i < nP; i++)
-		s[x] += (float)((2*ps[i*dimP+(x/dimP)]-1)*(2*ps[i*dimP+(x%dimP)]-1));
-	s[((x/dimP)*dimP)+(x/dimP)] = 0.0f;
-	ws[x] = s[x]/nP;
+	if (x < dimP*dimP){
+		product = 0.0f;
+		for (int i = 0; i < nP; i++)
+			product += (float)((2*ps[i*dimP+(x/dimP)]-1)*(2*ps[i*dimP+(x%dimP)]-1));
+		product = (!((((x/dimP)*dimP)+(x/dimP)) == x)) * product;
+		ws[x] = product/nP;
+	}
 }
 
 
 __global__ void hopActivation(int dimP, float *ws, int *pt, int *at)
 {
         extern __shared__ float sdata [];
-        int tid = blockDim.x*blockIdx.x+threadIdx.x;
-	int wpS = sizeWarp;
-	if (dimP < 32)
-		wpS = dimP; 
-        int wid = tid / wpS;
-        int lane= tid % wpS;
-        if (wid < wpS ){
+        int tid = blockDim.x*blockIdx.x+threadIdx.x; 
+        int wid  = tid / sizeWarp;
+        int lane = tid % sizeWarp;
+        if (wid < dimP ){
                 int start_neuron = (wid*dimP);
                 int end_neuron = ((wid+1)*dimP);
                 sdata[threadIdx.x]=0;
                 for(int i=start_neuron+lane;i<end_neuron;i+=32)
                         sdata[threadIdx.x]+= ws[i] * (2*pt[i % dimP ] -1);
 		__syncthreads();
-                if (lane + 16 < dimP) sdata[threadIdx.x] += sdata[threadIdx.x+16]; __syncthreads();
-		if (lane +  8 < dimP) sdata[threadIdx.x] += sdata[threadIdx.x+ 8]; __syncthreads();
-                if (lane +  4 < dimP) sdata[threadIdx.x] += sdata[threadIdx.x+ 4]; __syncthreads();
-                if (lane +  2 < dimP) sdata[threadIdx.x] += sdata[threadIdx.x+ 2]; __syncthreads();
-                if (lane +  1 < dimP) sdata[threadIdx.x] += sdata[threadIdx.x+ 1];
+                if (lane < 16) sdata[threadIdx.x] += sdata[threadIdx.x+16]; __syncthreads();
+		if (lane <  8) sdata[threadIdx.x] += sdata[threadIdx.x+ 8]; __syncthreads();
+                if (lane <  4) sdata[threadIdx.x] += sdata[threadIdx.x+ 4]; __syncthreads();
+                if (lane <  2) sdata[threadIdx.x] += sdata[threadIdx.x+ 2]; __syncthreads();
+                if (lane <  1) sdata[threadIdx.x] += sdata[threadIdx.x+ 1];
                 if (lane == 0)
 	        	at[wid] = ((sdata[threadIdx.x] > 0) - (sdata[threadIdx.x] < 0)+1)/2;
         }
@@ -55,16 +56,16 @@ float * lState (int nPatterns, int dimPattern, int *patterns)
 	int *ps;
 	float *weights, *ws;
 	int sizeP = dimPattern*sizeof(int);
-	int sizeW = dimPattern*dimPattern*sizeof(float);
+	int sizeW = dimPattern*dimPattern;
 
-	if ((weights = (float*) malloc (sizeW)) == NULL ) return NULL;
+	if ((weights = (float*) malloc (sizeW*sizeof(float))) == NULL ) return NULL;
 	if ( cudaSuccess != cudaMalloc ( &ps, (sizeP*nPatterns))) return NULL;
-	if ( cudaSuccess != cudaMalloc ( &ws, (sizeW))) return NULL;
+	if ( cudaSuccess != cudaMalloc ( &ws, (sizeW*sizeof(float)))) return NULL;
 	if ( cudaSuccess != cudaMemcpy (ps, patterns, sizeP*nPatterns, cudaMemcpyHostToDevice)) return NULL;
 
-	dim3 GRID_DIM (1);
-	dim3 BLOCK_DIM (dimPattern*dimPattern);
-	training<<< GRID_DIM, BLOCK_DIM, dimPattern*dimPattern*sizeof(float) >>> (dimPattern, nPatterns, ps, ws);
+	dim3 GRID_DIM ((sizeW+sizeBlock-1)/sizeBlock);
+	dim3 BLOCK_DIM (sizeBlock);
+	training<<< GRID_DIM, BLOCK_DIM, (sizeBlock)*sizeof(float) >>> (dimPattern, nPatterns, ps, ws);
   
 	if (cudaSuccess != cudaMemcpy (weights, ws, sizeW, cudaMemcpyDeviceToHost)) return NULL;
 	cudaFree(ps);
@@ -73,22 +74,22 @@ float * lState (int nPatterns, int dimPattern, int *patterns)
 }
 
 
-int * actFunc(int dP, int *pattern, float *weight)
+int * actFunc(int dimP, int *pattern, float *weight)
 {
 	float *ws;
 	int *pt, *activation, *at;
-	if ( (activation = (int *) malloc (dP*sizeof(int))) == NULL) return NULL;
-	if (cudaSuccess != cudaMalloc (&ws, dP*dP*sizeof(float))) return NULL;
-	if (cudaSuccess != cudaMalloc (&pt, dP*sizeof(int))) return NULL;
-	if (cudaSuccess != cudaMalloc (&at, dP*sizeof(int))) return NULL;
-	if ( cudaSuccess != cudaMemcpy (ws, weight, dP*dP*sizeof(float), cudaMemcpyHostToDevice)) return NULL;
-	if ( cudaSuccess != cudaMemcpy (pt, pattern, dP*sizeof(int), cudaMemcpyHostToDevice)) return NULL;
+	if ( (activation = (int *) malloc (dimP*sizeof(int))) == NULL) return NULL;
+	if (cudaSuccess != cudaMalloc (&ws, dimP*dimP*sizeof(float))) return NULL;
+	if (cudaSuccess != cudaMalloc (&pt, dimP*sizeof(int))) return NULL;
+	if (cudaSuccess != cudaMalloc (&at, dimP*sizeof(int))) return NULL;
+	if ( cudaSuccess != cudaMemcpy (ws, weight, dimP*dimP*sizeof(float), cudaMemcpyHostToDevice)) return NULL;
+	if ( cudaSuccess != cudaMemcpy (pt, pattern, dimP*sizeof(int), cudaMemcpyHostToDevice)) return NULL;
 
-	dim3 GRID_DIM (1);
-	dim3 BLOCK_DIM (dP*dP);
-	hopActivation<<< GRID_DIM, BLOCK_DIM, dP*dP*sizeof(float) >>> (dP, ws, pt, at);
+	dim3 GRID_DIM (((dimP*32)+sizeBlock-1)/sizeBlock);
+	dim3 BLOCK_DIM (sizeBlock);
+	hopActivation<<< GRID_DIM, BLOCK_DIM, sizeBlock*sizeof(float) >>> (dP, ws, pt, at);
 
-  	if (cudaSuccess != cudaMemcpy (activation, at, dP*sizeof(int), cudaMemcpyDeviceToHost)) return NULL;
+  	if (cudaSuccess != cudaMemcpy (activation, at, dimP*sizeof(int), cudaMemcpyDeviceToHost)) return NULL;
 	cudaFree(ws);
 	cudaFree(pt);
 	cudaFree(at);
